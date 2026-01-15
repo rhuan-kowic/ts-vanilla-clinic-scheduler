42c25d1d87cfadc3ed4622682918cf19192d087c
Rhuan Kowic <dev.rhuankowic@gmail.com>
2026-01-14 22:13:52 -0300

    chore(app): ajustar main para uso do Repository e Services

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,42 @@
 import "./styles.css";
-
 import { Repository } from "./core/Repository";
-import type { Cliente } from "./domain/Cliente";
-import type { Agendamento } from "./domain/Agendamento";
+import { type Cliente } from "./domain/Cliente";
 import { ClienteList } from "./components/ClienteList";
+import { ClienteService } from "./services/ClienteService"; 
 
-const clienteRepo = new Repository<Cliente>("clientes");
-
+const clienteRepo = new Repository<Cliente>("beauty_clientes_v1");
+const clienteService = new ClienteService(clienteRepo); 
 const clienteListComponent = new ClienteList("cliente-list-app");
-
-function atualizarTela() {
-  const dados = clienteRepo.getAll();
-  clienteListComponent.render(dados);
-}
-
 
 const form = document.getElementById("form-cliente") as HTMLFormElement;
 const inputNome = document.getElementById("nome") as HTMLInputElement;
 const inputTel = document.getElementById("telefone") as HTMLInputElement;
 const inputEmail = document.getElementById("email") as HTMLInputElement;
 
+function atualizarTela() {
+  const dados = clienteService.listarClientes();
+  clienteListComponent.render(dados);
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const novoCliente: Cliente = {
-    id: crypto.randomUUID(), 
-    nome: inputNome.value,
-    telefone: inputTel.value,
-    email: inputEmail.value,
-    dataCadastro: new Date()
-  };
-  
-  clienteRepo.add(novoCliente);
-  atualizarTela();
-  form.reset();
+  try {
+    clienteService.criarCliente({
+      nome: inputNome.value,
+      telefone: inputTel.value,
+      email: inputEmail.value
+    });
+
+    atualizarTela();
+    form.reset();
+    inputNome.focus();
+
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message);
+    }
+  }
 });
 
 atualizarTela();
