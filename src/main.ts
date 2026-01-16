92a1049c23ee1c891a31d0444ffbf56a32e94755
Rhuan Kowic <dev.rhuankowic@gmail.com>
2026-01-15 22:18:49 -0300

    feat(app): integrar router, serviços e fluxo de clientes

--- src/main.ts
+++ src/main.ts
@@ -2,41 +2,56 @@ import "./styles.css";
 import { Repository } from "./core/Repository";
 import { type Cliente } from "./domain/Cliente";
 import { ClienteList } from "./components/ClienteList";
-import { ClienteService } from "./services/ClienteService"; 
+import { ClienteService } from "./services/ClienteService";
+import { Router } from "./core/Router"; // <--- Importar Router
 
 const clienteRepo = new Repository<Cliente>("beauty_clientes_v1");
-const clienteService = new ClienteService(clienteRepo); 
+const clienteService = new ClienteService(clienteRepo);
 const clienteListComponent = new ClienteList("cliente-list-app");
+
+const viewClientes = document.getElementById("view-clientes") as HTMLDivElement;
+const viewAgenda = document.getElementById("view-agenda") as HTMLDivElement;
+
+function switchView(viewToShow: HTMLElement) {
+  viewClientes.classList.add("hidden");
+  viewAgenda.classList.add("hidden");
+
+  viewToShow.classList.remove("hidden");
+}
+
+const router = new Router();
+
+router.add("clientes", () => {
+  switchView(viewClientes);
+  const dados = clienteService.listarClientes();
+  clienteListComponent.render(dados);
+});
+
+router.add("agenda", () => {
+  switchView(viewAgenda);
+  console.log("Bem-vindo à Agenda!");
+});
 
 const form = document.getElementById("form-cliente") as HTMLFormElement;
 const inputNome = document.getElementById("nome") as HTMLInputElement;
 const inputTel = document.getElementById("telefone") as HTMLInputElement;
 const inputEmail = document.getElementById("email") as HTMLInputElement;
 
-function atualizarTela() {
-  const dados = clienteService.listarClientes();
-  clienteListComponent.render(dados);
-}
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   try {
     clienteService.criarCliente({
       nome: inputNome.value,
       telefone: inputTel.value,
-      email: inputEmail.value
+      email: inputEmail.value,
     });
 
-    atualizarTela();
-    form.reset();
-    inputNome.focus();
+    const dados = clienteService.listarClientes();
+    clienteListComponent.render(dados);
 
+    form.reset();
+    alert("Cliente cadastrado!");
   } catch (error) {
-    if (error instanceof Error) {
-      alert(error.message);
-    }
+    if (error instanceof Error) alert(error.message);
   }
 });
-
-atualizarTela();
